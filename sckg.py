e26998fc60e83f2312978d18558e92b9ca2e7d93
Jason Callaway <jasoncallaway@google.com>
2019-11-27 23:29:20 -0500

    fleshed out dynamic class instantiator

--- sckg.py
+++ sckg.py
@@ -1,8 +1,30 @@
+import importlib
+import yaml
 
 class SCKG(object):
 
   def __init__(self):
-    pass
+    with open('config.yml', 'r') as f:
+      self.config = yaml.safe_load(f.read())
+
+    for regime in self.config['regimes']:
+      self.regimeETL(regime)
+
+  def regimeETL(self, regime):
+    etl_config = regime.get('etl', 'generic')
+    if etl_config == 'generic':
+      module_name = self.config['defaults']['generic']['module']
+      class_name = self.config['defaults']['generic']['class']
+    else:
+      module_name = regime['etl']['module']
+      class_name = regime['etl']['class']
+    etl_class = getattr(importlib.import_module(module_name),
+                        class_name)
+    etl_instance = etl_class(self.config)
+
+    etl_instance.extract()
+    etl_instance.transform()
+    etl_instance.load()
 
 def main():
   sckg = SCKG()
