14623aa99f2ddaec22080dc45bfb23c4c7e2777f
Jason Callaway <jason@jasoncallaway.com>
2019-11-29 19:37:07 -0500

    added cis logic

--- sckg/etl/cis.py
+++ sckg/etl/cis.py
@@ -7,5 +7,80 @@ class CIS(Generic):
     super().__init__(config)
 
   def transform(self, regime, regime_list):
-    return []
+    stmts = []
+    regime_name = regime['description']
+    stmts.append(self.create_regime(regime_name))
+
+    # create the controls and families
+    for control_dict in regime_list:
+      c = control_dict.copy()
+      c = self.clean_dict(c)
+      if not c.get('cis_sub_control'):
+        properties = {'name': c['cis_control'],
+                      'title': c['title']}
+        stmts.append(
+            self.create_regime_family(regime_name,
+                                      properties=properties)
+        )
+      else:
+        properties = {'name': c['cis_sub_control'],
+                      'title': c['title'],
+                      'description': c.get('description', '')}
+        stmts.append(
+            self.create_geneirc_control(regime_name,
+                                        'family',
+                                        c['cis_control'],
+                                        properties=properties)
+        )
+
+    # create the baselines
+    for control_dict in regime_list:
+      c = control_dict.copy()
+      c = self.clean_dict(c)
+      baselines = {'implementation_group_1': 'Implementation Group 1',
+                   'implementation_group_2': 'Implementation Group 2',
+                   'implementation_group_3': 'Implementation Group 3'}
+      for key in baselines.keys():
+        if c.get(key):
+          stmts.append(
+              self.create_regime_baseline(regime_name,
+                                          properties={
+                                            'name': baselines[key]
+                                          })
+          )
+          stmts.append(
+              self.create_baseline_control(regime_name,
+                                           baselines[key],
+                                           regime_name,
+                                           c['cis_sub_control'],
+                                           properties={})
+          )
+
+      baselines = {'asset_type': 'Asset Type',
+                   'security_function': 'Security Function'}
+
+      for key in baselines.keys():
+        if c.get(key):
+          stmts.append(
+              self.create_regime_baseline(regime_name,
+                                          properties={
+                                            'name': baselines[key]
+                                          })
+          )
+
+          baseline_baseline_properties = {'name': c[key]}
+          stmts.append(
+              self.create_baseline_baseline(regime_name,
+                                            baselines[key],
+                                            properties=baseline_baseline_properties)
+          )
+          stmts.append(
+              self.create_baseline_control(regime_name,
+                                           c[key],
+                                           regime_name,
+                                           c['cis_sub_control'],
+                                           properties={})
+          )
+
+    return stmts
 
