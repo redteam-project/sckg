66555021a847bcb3aa352f95cabcedff1387642a
Jason Callaway <jason@jasoncallaway.com>
2019-11-29 19:03:09 -0500

    list concat stupidity and nist csf doc path typo

--- sckg/etl/generic.py
+++ sckg/etl/generic.py
@@ -22,11 +22,11 @@ class Generic(object):
     if regime.get('baseline'):
       baseline = regime['baseline']
 
-      stmts.append(self.create_generic_baseline(baseline['regime_name'],
-                                                self.get_control_regime_name(regime),
-                                                baseline['baseline_name'],
-                                                baseline['uid_key'],
-                                                regime_list))
+      stmts += self.create_generic_baseline(baseline['regime_name'],
+                                            self.get_control_regime_name(regime),
+                                            baseline['baseline_name'],
+                                            baseline['uid_key'],
+                                            regime_list)
       return stmts
     else:
       raise Exception('no baseline for regime "{}" in config.yml'.format(regime['name']))
