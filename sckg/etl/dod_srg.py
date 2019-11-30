8b46bacfaaeacf1c13ae0c7b9b1e8a8f4812541f
Jason Callaway <jason@jasoncallaway.com>
2019-11-29 19:20:47 -0500

    added dod srg logic

--- sckg/etl/dod_srg.py
+++ sckg/etl/dod_srg.py
@@ -6,6 +6,119 @@ class DoDSRG(Generic):
   def __init__(self, config):
     super().__init__(config)
 
+  def parameter_search(self, r, c):
+    for x in r:
+      if x.get('parameters'):
+        if x['control'] == c:
+          x = self.clean_dict(x)
+          return x.get('parameters')
+    return ''
+
   def transform(self, regime, regime_list):
-    return []
+    stmts = []
+    regime_name = regime['description']
+    stmts.append(
+        self.create_regime(regime_name)
+    )
+    stmts.append(
+        self.create_regime_baseline(regime_name,
+                                    properties={'name': 'Impact Level 4'})
+    )
+    stmts.append(
+        self.create_regime_baseline(regime_name,
+                                    properties={'name': 'Impact Level 5'})
+    )
+    stmts.append(
+        self.create_regime_baseline(regime_name,
+                                    properties={'name': 'Impact Level 6'})
+    )
+    stmts.append(
+        self.create_regime_baseline(regime_name,
+                                    properties={'name': 'Sections'})
+    )
+    stmts.append(
+        self.create_baseline_baseline(regime_name,
+                                      'Sections',
+                                      properties={'name': '5'})
+    )
+    stmts.append(
+        self.create_baseline_baseline(regime_name,
+                                      'Sections',
+                                      properties={'name': '6'})
+    )
+
+    for control_dict in regime_list:
+      c = self.clean_dict(control_dict.copy())
+      if c.get('section'):
+        if not 'DoD-SRG' in c['control']:
+          # it's a NIST 800-53 reference
+          if c.get('level_4'):
+            stmts.append(
+                self.create_baseline_control(regime_name,
+                                             'Impact Level 4',
+                                             'NIST 800-53',
+                                             c['control'],
+                                             properties={
+                                               'section': c['section'],
+                                               'parameters': self.parameter_search(
+                                                   regime_list,
+                                                   c['control'])
+                                             })
+            )
+          if c.get('level_5'):
+            stmts.append(
+                self.create_baseline_control(regime_name,
+                                             'Impact Level 5',
+                                             'NIST 800-53',
+                                             c['control'],
+                                             properties={
+                                               'section': c['section'],
+                                               'parameters': self.parameter_search(
+                                                   regime_list,
+                                                   c['control'])
+                                             })
+            )
+          if c.get('level_6'):
+            stmts.append(
+                self.create_baseline_control(regime_name,
+                                             'Impact Level 6',
+                                             'NIST 800-53',
+                                             c['control'],
+                                             properties={
+                                               'section': c['section'],
+                                               'parameters': self.parameter_search(
+                                                   regime_list,
+                                                   c['control'])
+                                             })
+            )
+        else:
+          # it's an SRG narrative control
+          section_parts = c['section'].split('.')
+          handled_parts = []
+          for part in section_parts:
+            if len(handled_parts) > 0:
+              stmts.append(
+                  self.create_baseline_baseline(regime_name,
+                                                '.'.join(handled_parts),
+                                                properties={
+                                                  'name': '.'.join(
+                                                      handled_parts) + '.' + part
+                                                })
+              )
+            handled_parts.append(part)
+          parent_parts = section_parts[:-1]
+          parent_name = '.'.join(parent_parts)
+          stmts.append(
+              self.create_baseline_has_control(regime_name,
+                                               c['section'],
+                                               properties={
+                                                 'name': c['control'],
+                                                 'requirement': c[
+                                                   'requirement']
+                                               })
+          )
+      elif c.get('parameters'):
+        pass
+
+    return stmts
 
