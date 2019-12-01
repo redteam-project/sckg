6861ba7a4fb8d2e6320ff62d4904637c600145f4
Jason Callaway <jason@jasoncallaway.com>
2019-11-30 20:19:51 -0500

    added cci logic and a few more bash scripts

--- sckg/etl/dod_cci.py
+++ sckg/etl/dod_cci.py
@@ -10,47 +10,71 @@ class DoDCCI(Generic):
     super().__init__(config)
 
   def extract(self, regime, parsable_document):
-    # with open(parsable_document, 'r') as f:
-    #   xml = f.read()
-    #
-    # root = objectify.fromstring(xml)
-    #
-    # j = []
-    # for item in root.cci_items.cci_item:
-    #   id = item.attrib.get('id', '')
-    #   control_list = []
-    #   for reference in item.references.reference:
-    #     control_list.append(reference.attrib.get('index'))
-    #   j.append({
-    #     'id': id,
-    #     'controls': control_list
-    #   })
-    #
-    # return j
-    return []
+    with open(parsable_document, 'r') as f:
+      xml = f.read()
+
+    root = objectify.fromstring(xml)
+
+    j = []
+    for item in root.cci_items.cci_item:
+      id = item.attrib.get('id', '')
+      control_list = []
+      for reference in item.references.reference:
+        control_list.append(reference.attrib.get('index'))
+      j.append({
+        'id': id,
+        'controls': control_list
+      })
+
+    return j
 
   def transform(self, regime, regime_list):
-    # regime_name = regime['description']
-    #
-    # stmts = []
-    # stmts.append(self.create_regime(regime_name))
-    # stmts.append(
-    #     self.create_regime_baseline(regime_name,
-    #                                 properties={
-    #                                   'name': 'CCI'
-    #                                 })
-    # )
-    # for cci in regime_list:
-    #   cci_name = list(cci.keys())[0]
-    #   cci_controls = cci[cci_name].split(',')
-    #   stmts.append(
-    #       self.create_baseline_control(regime_name,
-    #                                    'baseline',
-    #                                    'CCI',
-    #                                    regime_name,
-    #                                    cci_name)
-    #   )
-    #
-    return []
+    regime_name = regime['description']
+    updated = regime['meta']['updated']
+
+    stmts = []
+    stmts.append(self.create_regime(regime_name))
+    stmts.append(
+        self.create_regime_baseline(regime_name,
+                                    properties={
+                                      'name': updated
+                                    })
+    )
+    for cci in regime_list:
+      name = cci['id']
+      controls = cci['controls']
+      stmts.append(
+          self.create_geneirc_control(regime_name,
+                                      'baseline',
+                                      updated,
+                                      properties={
+                                        'name': name
+                                      })
+      )
+      corrected_controls = []
+      for control in controls:
+        if ' ' in control and '(' not in control:
+          control = control.replace(' ', '', 1).replace(' ', '.') + '.'
+          corrected_controls.append(control)
+        else:
+          corrected_controls.append(control)
+      for control in corrected_controls:
+        stmts.append(
+            self.create_control_control_map(
+                names={
+                  'by_regime': True,
+                  'mapping_regime': 'DoD CCI',
+                  'mapping_control': name,
+                  'mapped_regime': 'NIST 800-53',
+                  'mapped_control': control,
+                  'relationship': 'REFERENCES'
+                },
+                properties={}
+            )
+        )
+
+    return stmts
+
+
 
 
