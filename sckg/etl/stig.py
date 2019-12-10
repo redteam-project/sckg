18dc5289b6d8d9d3626ae7dd98bd13a9f8984165
Jason Callaway <jasoncallaway@google.com>
2019-12-10 18:16:01 -0500

    added niap gpos and disa stigs that map to ccis

--- sckg/etl/stig.py
+++ sckg/etl/stig.py
@@ -15,20 +15,57 @@ class STIG(Generic):
 
     rules = []
     for r in root.iter():
-      for group in r.Group:
-        for rule in group.Rule:
-          d = {}
-          d['id'] = rule.attrib.get('id', None)
-          d['severity'] = rule.attrib.get('severity', None)
-          d['weight'] = rule.attrib.get('weight', None)
-          d['check'] = rule.check.attrib.get('system', None)
-          d['description'] = rule.description.text
-          d['fix'] = rule.fix.attrib.get('id', None)
-          d['ident'] = rule.ident.text
-          d['title'] = rule.title.text
-          d['version'] = rule.version.text
-          rules.append(d)
-
-      pause = True
+      if 'Benchmark' in r.tag:
+        for group in r.Group:
+          for rule in group.Rule:
+            d = {
+              'name': rule.attrib.get('id', None),
+              'severity': rule.attrib.get('severity', None),
+              'weight': rule.attrib.get('weight', None),
+              'check': rule.check.attrib.get('system', None),
+              'description': rule.description.text,
+              'fix': rule.fix.attrib.get('id', None),
+              'ident': rule.ident.text,
+              'title': rule.title.text,
+              'version': rule.version.text
+            }
+            rules.append(d)
 
     return rules
+
+  def transform(self, regime, regime_list):
+    regime_name = 'STIG'
+    stig_name = regime['description']
+
+    stmts = []
+    stmts.append(self.create_regime(regime_name))
+    stmts.append(
+        self.create_regime_baseline(regime_name,
+                                    properties={
+                                      'name': stig_name
+                                    })
+    )
+    for control in regime_list:
+      control = self.clean_dict(control)
+      stmts.append(
+          self.create_baseline_has_control(regime_name,
+                                           stig_name,
+                                           properties=control)
+      )
+      stmts.append(
+          self.create_control_control_map(
+            names={
+                'by_baseline': True,
+                'mapping_regime': regime_name,
+                'mapping_baseline': stig_name,
+                'mapping_control': control['name'],
+                'mapped_regime': regime['baseline']['control_regime'],
+                'mapped_baseline': regime['baseline']['control_baseline'],
+                'mapped_control': control['ident'],
+                'relationship': 'REFERENCES'
+            },
+            properties={}
+          )
+      )
+
+    return stmts
